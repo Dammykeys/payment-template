bb5df8e7d73a88644460ddcb2f22103ea7d1d4dc
Damilola <immanueladeniyi7@gmail.com>
2023-02-05 14:17:39 +0100

    Update script.js

--- js/script.js
+++ js/script.js
@@ -11,3 +11,12 @@ function scrollToTop() {
         behavior: "smooth",
     });
 }
+
+//Preloader 
+window.addEventListener("load", () => {
+    const preloader = document.querySelector(".preloader");
+    preloader.classList.add("preloader-hidden");
+    preloader.addEventListener("transitionend", () => {
+        document.body.removeChild(preloader);
+    })
+});
