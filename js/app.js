790a78f18a6c23595b7891ff41799887a0b8d63f
Mitchel-JF <mitchellcorporations@gmail.com>
2023-03-15 05:14:31 +0100

    sw file

--- js/app.js
+++ js/app.js
@@ -1,7 +1,7 @@
 //Service Worker
 if (navigator.serviceWorker) {
   window.addEventListener('load', () => {
-    navigator.serviceWorker.register('/sw.js').then(reg => {
+    navigator.serviceWorker.register('sw.js').then(reg => {
     }).catch(swErr => {
       console.log(`Service Worker Installation Error: ${swErr}}`)
     });
