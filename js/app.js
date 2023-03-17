5194be2fe2be33f25ffc8499ca8effda8455082a
Mitchel-JF <mitchellcorporations@gmail.com>
2023-03-17 14:27:07 +0100

    sw is working fine

--- js/app.js
+++ js/app.js
@@ -1,9 +1,11 @@
 //Service Worker
 if (navigator.serviceWorker) {
   window.addEventListener('load', () => {
-    navigator.serviceWorker.register('sw.js').then(reg => {
+    navigator.serviceWorker.register('../sw.js').then(reg => {
     }).catch(swErr => {
       console.log(`Service Worker Installation Error: ${swErr}}`)
     });
   });
+} else {
+  console.log('No service-worker on this browser');
 };
