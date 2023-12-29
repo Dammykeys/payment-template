237b98a8e005886578b93030b212e215d0926ac1
Mitchel-JF <mitchellcorporations@gmail.com>
2023-12-29 14:48:51 +0100

    adjustments to updates

--- js/payment.js
+++ js/payment.js
@@ -1,31 +1,3 @@
-<<<<<<< HEAD
-//paystack payment
-const paymentForm = document.getElementById('paymentForm');
-paymentForm.addEventListener("submit", payWithPaystack, false);
-function payWithPaystack(e) {
-   e.preventDefault();
-
-   let handler = PaystackPop.setup({
-      key: 'pk_test_7273e0d3e51db20b949946c707d2180a7af72bda',
-      email: document.getElementById("email-address").value,
-      amount: document.getElementById("amount").value * 100,
-      ref: '' + Math.floor((Math.random() * 1000000000) + 1),
-
-      // label: "Optional string that replaces customer email"
-      onClose: function () {
-         alert('Oops! Are you sure you want to terminate transaction?');
-         document.getElementById('transaction-id').value = ref;
-      },
-      callback: function (response) {
-         let message = 'Payment complete! Reference: ' + response.reference;
-         alert(message);
-      }
-   });
-   handler.openIframe();
-}
-
-export { payWithPaystack };
-=======
 //paystack payment
 const paymentForm = document.getElementById('paymentForm');
 var amountField = document.getElementById('amount');
@@ -92,4 +64,3 @@ export { payWithPaystack };
 // })
 
 // .replace(/,/g, '')   => to remove ,
->>>>>>> fac9df237b8ddea8b8dea1b40d9888de362ae631
