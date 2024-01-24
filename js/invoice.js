43826b9768ed2c3cb7b9fea194479a634c74dd2a
Adeniyi Damilola <immanueladeniyi7@gmail.com>
2024-01-24 17:05:03 +0100

    new auto logout function

--- js/invoice.js
+++ js/invoice.js
@@ -1,6 +1,21 @@
 let UserCreds = JSON.parse(sessionStorage.getItem("user-creds"));
 let UserInfo = JSON.parse(sessionStorage.getItem("user-info"));
+var frequency = null;
 
+
+window.onblur = function () {
+   enableAutoLogout();
+};
+window.onfocus = function () {
+   if (frequency !== null) {
+      disableAutoLogout();
+   };
+};
+window.onclick = function () {
+   if (frequency !== null) {
+      disableAutoLogout();
+   };
+};
 
 let Signout = () => {
    sessionStorage.removeItem('user-creds');
@@ -16,11 +31,18 @@ let checkCreds = () => {
    };
 };
 
-setTimeout(function () {
-   sessionStorage.removeItem('user-creds');
-   sessionStorage.removeItem('user-info');
-   alert("Session has Expired!");
-   window.location.href = "login.html";
-}, 900000);
+function enableAutoLogout() {
+   frequency = setTimeout(function () {
+      sessionStorage.removeItem('user-creds');
+      sessionStorage.removeItem('user-info');
+      alert("Session has Expired!");
+      window.location.href = "login.html";
+   }, 900000);
+};
+
+function disableAutoLogout() {
+   clearTimeout(frequency);
+   frequency = null;
+}
 
 window.addEventListener('load', checkCreds);
